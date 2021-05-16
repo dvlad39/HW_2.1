a76b3708fc1a4896840ef6e1ed6a51304c93e25d
Vladyslav Shreder <dvlad39@gmail.com>
2021-05-16 16:01:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW_2
+{
+
+    class Triangle
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            for (int i = 0; i <= 12; i += 2)
+            {
+                for (int j = 0; j < 12 - i - 1; j += 2)
+                {
+                    Console.Write("  ");
+                }
+                for (int x = 12 - i - 1; x < 12; x++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
+
